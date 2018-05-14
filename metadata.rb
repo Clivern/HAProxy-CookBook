4b1bc3d1d292b2628d36fa4a77cc9be4b7da86b2
Clivern <hello@clivern.com>
2018-05-14 23:00:58 +0200

    Init cookbook structure

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name 'haproxy-cookbook'
+maintainer 'Clivern'
+maintainer_email 'hello@clivern.com'
+license 'Apache 2.0'
+description 'Installs and Configures HAProxy'
+long_description 'Installs and Configures HAProxy'
+version '1.0.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
